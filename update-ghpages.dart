b5d5c9b89bb8d1520c284c1b4d76331350474abd
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2014-04-25 10:15:08 +0200

    update gh-pages generator script

--- update-ghpages.dart
+++ update-ghpages.dart
@@ -2,7 +2,8 @@ import 'package:ghpages_generator/ghpages_generator.dart' as gh;
 
 main() {
   new gh.Generator()
-  ..setDartDoc(['lib/ghpages_generator.dart'], excludedLibs: ['path'])
-  ..templateDir = 'gh-pages-template'
-  ..generate();
+      ..setDartDoc(['lib/ghpages_generator.dart'], includeSdk: false,
+          excludedLibs: ['path'], startPage: 'ghpages_generator')
+      ..templateDir = 'gh-pages-template'
+      ..generate();
 }
