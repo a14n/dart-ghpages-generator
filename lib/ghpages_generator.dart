947a0e9c24aa05dcceb9f41c1c4c3864de7248f4
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2015-01-12 09:43:16 +0100

    ignore .pub

--- lib/ghpages_generator.dart
+++ lib/ghpages_generator.dart
@@ -219,7 +219,7 @@ class Generator {
         });
       })
       .then((_) {
-        _delete(_workDir, ['build', 'packages', 'lib', 'pubspec.yaml', 'pubspec.lock', 'dartdoc-viewer']);
+        _delete(_workDir, ['build', 'packages', 'lib', 'pubspec.yaml', 'pubspec.lock', 'dartdoc-viewer', '.pub']);
       })
       .then((_) {
         if (_templateDir != null) {
